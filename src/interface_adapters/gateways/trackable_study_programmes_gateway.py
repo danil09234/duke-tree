09378f32354e454cd37752f0cd1eb25657bfce87
danil09234 <dailzag05@gmail.com>
2024-09-28 03:00:40 +0200

    Add tests for `StudyProgrammesGateway`
    Additionally:
    - Rename `StudyProgrammeGateway` -> `StudyProgrammesGateway`
    - Extend fixture

--- src/interface_adapters/gateways/trackable_study_programmes_gateway.py
+++ src/interface_adapters/gateways/trackable_study_programmes_gateway.py
@@ -2,7 +2,7 @@ from typing import Awaitable, Callable, Any
 
 from src.application.interfaces import WebPageLoader, Parser
 from src.domain.entities.study_programme import StudyProgramme
-from src.interface_adapters.gateways.study_programme_gateway import StudyProgrammesGateway
+from src.interface_adapters.gateways.study_programmes_gateway import StudyProgrammesGateway
 
 
 class TrackableStudyProgrammeGateway(StudyProgrammesGateway):
@@ -12,5 +12,5 @@ class TrackableStudyProgrammeGateway(StudyProgrammesGateway):
         self._gathering_function = gathering_function
 
     async def get_by_codes(self, programmes_codes: list[str]) -> list[StudyProgramme]:
-        pages = await self._gathering_function(*await self._get_all_pages_loading_coroutines(programmes_codes))
+        pages = await self._gathering_function(*await self._get_all_pages_loading_coroutines_generator(programmes_codes))
         return self._parser.parse_multiple(pages)
---
+++ tests/interface_adapters/gateways/test_study_programmes_gateway.py
@@ -0,0 +1,66 @@
+from unittest.mock import create_autospec, AsyncMock, Mock
+
+import pytest
+
+from src.interface_adapters.exceptions import PageLoadingError
+from src.domain.enums import Language
+from src.domain.entities import StudyProgramme
+from src.application.interfaces import WebPageLoader, Parser
+from src.interface_adapters.gateways.study_programmes_gateway import StudyProgrammesGateway
+
+
+@pytest.mark.asyncio
+async def test_get_by_codes(test_codes: list[str], test_study_programmes: list[StudyProgramme]) -> None:
+    url_template = "https://res.tuke.sk/api/programme_detail/{code}?lang={lang}"
+
+    loader_mock = create_autospec(WebPageLoader, load=AsyncMock(), spec_set=True)
+
+    parse_multiple_mock = Mock()
+    parse_multiple_mock.return_value = test_study_programmes
+
+    parse_one_mock = Mock(side_effect=test_study_programmes)
+
+    study_programmes_source_mock = create_autospec(
+        Parser[str, StudyProgramme], parse_one=parse_one_mock, parse_multiple=parse_multiple_mock, spec_set=True
+    )
+
+    gateway = StudyProgrammesGateway(loader=loader_mock, parser=study_programmes_source_mock)
+    gateway_return_values = await gateway.get_by_codes(test_codes)
+    assert gateway_return_values == test_study_programmes
+
+    for test_code in test_codes:
+        for language in Language:
+            test_url = url_template.format(code=test_code, lang=language.value)
+            loader_mock.load.assert_any_call(test_url)
+
+
+@pytest.mark.asyncio
+async def test_get_by_codes_with_exception_during_page_loading(
+        test_codes: list[str], test_study_programmes: list[StudyProgramme]
+) -> None:
+    successfully_loaded_pages = [f"page {n} content" for n in range(len(Language) * len(test_codes) - 1)]
+    returned_pages = [
+        PageLoadingError(),
+        *successfully_loaded_pages
+    ]
+    loader_mock = create_autospec(WebPageLoader, load=AsyncMock(side_effect=returned_pages), spec_set=True)
+
+    parse_multiple_mock = Mock()
+    parse_one_mock = Mock()
+
+    study_programmes_source_mock = create_autospec(
+        Parser[str, StudyProgramme], parse_one=parse_one_mock, parse_multiple=parse_multiple_mock, spec_set=True
+    )
+
+    gateway = StudyProgrammesGateway(loader=loader_mock, parser=study_programmes_source_mock)
+    await gateway.get_by_codes(test_codes)
+
+    try:
+        parse_multiple_mock.assert_called_once_with(successfully_loaded_pages)
+    except AssertionError:
+        for page in successfully_loaded_pages:
+            parse_one_mock.assert_any_call(page)
+
+        assert parse_one_mock.call_count == len(successfully_loaded_pages), \
+            (f"Expected parse_one_mock to be called {len(successfully_loaded_pages)} times, "
+             f"but it was called {parse_one_mock.call_count} times.")
