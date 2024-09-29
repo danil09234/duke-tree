6e994569b471d9bd8da5806416145962995ad009
Vladyslav Panik <vpanik50@gmail.com>
2024-09-29 21:46:48 +0200

    Implementation of study programmes code extraccting from .xlsx file
    3. point in #1

--- src/interface_adapters/persistence/study_programmes_codes_excel_repository.py
+++ src/interface_adapters/persistence/study_programmes_codes_excel_repository.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import openpyxl
+
 from src.application.interfaces import Fetchable
 
 
@@ -8,10 +10,22 @@ class StudyProgrammesCodesExcelRepository(Fetchable[str]):
         self._file_path = file_path
 
     async def fetch_all(self) -> list[str]:
-        """
-        Fetches study programmes codes from an Excel file.
+        programme_codes = []
+        wb = openpyxl.load_workbook(self._file_path)
+        sheet = wb.active
+        max_rows = sheet.max_row
 
-        :return: List of study programmes codes.
-        """
-        # TODO: Implement fetching study programmes codes from an Excel file.
-        raise NotImplementedError
+        # Check if the first row is a header
+        first_row = [cell.value for cell in sheet[1]]
+        has_header = any(isinstance(cell, str) for cell in first_row)
+        start_row = 2 if has_header else 1
+
+        for i in range(start_row, max_rows + 1):
+            code_cell = sheet.cell(row=i, column=1).value
+            if not code_cell:
+                continue
+            programme_codes.append(code_cell)
+        programme_codes.sort()
+        programme_codes = [str(code) for code in programme_codes]
+
+        return programme_codes
--- src/main.py
+++ src/main.py
@@ -1,4 +1,21 @@
+import asyncio
+from pathlib import Path
+
+from interface_adapters.persistence.study_programmes_codes_excel_repository import StudyProgrammesCodesExcelRepository
 from src.infrastructure.cli.app import cli
 
-if __name__ == "__main__":
+
+async def main():
     cli()
+
+    # Test extracting study programmes codes from an Excel file
+
+    # file_path = Path("../resources/study_programmes.xlsx")
+    # repository = StudyProgrammesCodesExcelRepository(file_path)
+    # study_programmes_codes = await repository.fetch_all()
+    # with open("programme_codes.txt", "w") as f:
+    #     for code in study_programmes_codes:
+    #         f.write(f"{code}\n")
+
+if __name__ == "__main__":
+    asyncio.run(main())
