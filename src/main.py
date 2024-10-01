c9c83b2436546d0e52f9a1ee030d7b0c47adb576
Vladyslav Panik <vpanik50@gmail.com>
2024-10-01 09:32:44 +0200

    Prepared files to PR

--- src/main.py
+++ src/main.py
@@ -6,13 +6,5 @@ from src.infrastructure.cli.app import cli
 async def main() -> None:
     cli()
 
-    # Test extracting study programmes codes from an Excel file
-    # file_path = Path("../resources/study_programmes.xlsx")
-    # repository = StudyProgrammesCodesExcelRepository(file_path)
-    # study_programmes_codes = await repository.fetch_all()
-    # with open("programme_codes.txt", "w") as f:
-    #     for code in study_programmes_codes:
-    #         f.write(f"{code}\n")
-
 if __name__ == "__main__":
     asyncio.run(main())
