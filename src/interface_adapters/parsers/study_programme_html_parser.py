fd56ed87852e2971ae82e93973d8de4e2044b53c
danil09234 <dailzag05@gmail.com>
2024-10-19 02:36:54 +0200

    Refactor code

--- src/interface_adapters/parsers/study_programme_html_parser.py
+++ src/interface_adapters/parsers/study_programme_html_parser.py
@@ -3,7 +3,7 @@ from src.application.interfaces import Parser
 from src.domain.enums.degree import Degree
 from src.domain.enums.languages import Language
 from src.domain.enums.study_form import StudyForm
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 
 class StudyProgrammeHtmlParser(Parser[str, TukeStudyProgramme]):
@@ -20,35 +20,107 @@ class StudyProgrammeHtmlParser(Parser[str, TukeStudyProgramme]):
         data = self._extract_data_from_page(soup)
         return self._create_study_programme(data)
 
+    def _extract_data_from_page(self, soup: BeautifulSoup) -> dict:
+        """
+        Extracts data from the HTML page content.
+
+        :param soup: Parsed HTML content.
+        :return: Dictionary containing study programme data.
+        """
+        data = {}
+        data.update(self._extract_data_from_section(soup, 'div', 'col-md-8 main'))
+        data.update(self._extract_data_from_section(soup, 'div', 'col-md-4 aside'))
+        return data
+
+    def _extract_data_from_section(self, soup: BeautifulSoup, tag: str, class_name: str) -> dict[str, str]:
+        """
+        Extracts table data from a specific section (tag and class).
+
+        :param soup: Parsed HTML content.
+        :param tag: Tag name where the table is located.
+        :param class_name: Class name where the table is located.
+        :return: Dictionary with extracted key-value pairs.
+        """
+        section = soup.find(tag, class_=class_name)
+        if not section:
+            return {}
+
+        table = section.find('table', class_='table')
+        if not table:
+            return {}
+
+        return self._extract_table_data(table)
+
     @staticmethod
-    def _map_to_joint_study_program(joint_program: str) -> bool:
+    def _extract_table_data(table: Tag) -> dict[str, str]:
         """
-        Maps a string to a Joint study program bool value
+        Extracts data from an HTML table.
 
-        :param joint_program: String to map.
-        :return: bool value ("True" or "False").
+        :param table: BeautifulSoup object representing the HTML table.
+        :return: Dictionary with extracted key-value pairs.
         """
-        if joint_program.lower() == 'áno' or joint_program.lower() == 'yes':
-            return True
-        elif joint_program.lower() == 'nie' or joint_program.lower() == 'no':
-            return False
-        else:
-            raise ValueError(f"Unknown Joint study program value: {joint_program}")
+        data = {}
+        for row in table.find_all('tr'):
+            th = row.find('th')
+            td = row.find('td')
+
+            if th and td:
+                key = th.text.strip()
+                value = td.text.strip()
+                if key not in data:
+                    data[key] = value
+        return data
+
+    def _create_study_programme(self, data) -> TukeStudyProgramme:
+        """
+            Creates a TukeStudyProgramme object from the extracted data.
+
+            :param data: Dictionary containing study programme data.
+            :return: TukeStudyProgramme object.
+        """
+        return TukeStudyProgramme(
+            page_url=self._get_page_url(data),
+            page_language=self._map_to_language(data.get("Jazyky poskytovania:", "") or
+                                                data.get("Languages of provision:", "")),
+            name=data.get("Študijný program:", "") or data.get("Study program:", ""),
+            programme_code=int(data.get("Kód:", "") or data.get("Code:", "")),
+            study_field=data.get("Študijný odbor:", "") or data.get("Field of study:", ""),
+            level_of_degree=int((data.get("Stupeň štúdia:", "") or data.get("Level of study:", "")).replace(".", "")),
+            study_form=self._map_to_study_form(data.get("Forma štúdia:", "") or data.get("Form of study:", "")),
+            degree=self._map_to_degree(data.get("Stupeň štúdia:", "") or data.get("Level of study:", "")),
+            length_of_study_in_years=int(data.get("Dĺžka štúdia:", "") or data.get("Length of study:", "")),
+            professionally_oriented=self._map_to_professionally_oriented(data.get("Profesijne orientovaný:", "")
+                                                                         or data.get("Professionally oriented:", "")),
+            joint_study_program=self._map_to_joint_study_program(data.get("Spoločný študijný program:", "")
+                                                                 or data.get("Common study program:")),
+            languages_of_delivery=self._map_to_language(data.get("Jazyky poskytovania:", "") or
+                                                        data.get("Languages of provision:", "")),
+            description="",
+            learning_objectives="",
+            main_learning_outcomes="",
+            faculty=data.get("Fakulta:", "") or data.get("Faculty:", "")
+        )
+
+    @classmethod
+    def _get_page_url(cls, data: dict[str, str]) -> str:
+        study_programme_code = data.get("Kód:", "") or data.get("Code:", "")
+        language = "en" if "Code:" in data else "sk"
+        return cls._PROGRAMME_PAGE_URL_TEMPLATE.format(code=study_programme_code, lang=language)
 
     @staticmethod
-    def _map_to_professionally_oriented(oriented: str) -> bool:
+    def _map_to_language(language: str) -> Language:
         """
-        Maps a string to a Professionally oriented bool value
+        Maps a string to a Language enum.
 
-        :param oriented: String to map.
-        :return: bool value ("True" or "False").
+        :param language: String to map.
+        :return: Language enum.
         """
-        if oriented.lower() == 'áno' or oriented.lower() == 'yes':
-            return True
-        elif oriented.lower() == 'nie' or oriented.lower() == 'no':
-            return False
+        if language.lower() == 'anglický jazyk' or language.lower() == 'anglicky jazyk':
+            return Language.ENGLISH
+        elif language.lower() == 'slovenský jazyk' or language.lower() == 'slovensky jazyk':
+            return Language.SLOVAK
         else:
-            raise ValueError(f"Unknown professionally oriented value: {oriented}")
+            raise ValueError(f"Unknown language: {language}")
 
     @staticmethod
     def _map_to_study_form(study_form: str) -> StudyForm:
@@ -83,103 +155,31 @@ class StudyProgrammeHtmlParser(Parser[str, TukeStudyProgramme]):
             raise ValueError(f"Unknown degree: {degree}")
 
     @staticmethod
-    def _map_to_language(language: str) -> Language:
+    def _map_to_professionally_oriented(oriented: str) -> bool:
         """
-        Maps a string to a Language enum.
+        Maps a string to a Professionally oriented bool value
 
-        :param language: String to map.
-        :return: Language enum.
+        :param oriented: String to map.
+        :return: bool value ("True" or "False").
         """
-        if language.lower() == 'anglický jazyk' or language.lower() == 'anglicky jazyk':
-            return Language.ENGLISH
-        elif language.lower() == 'slovenský jazyk' or language.lower() == 'slovensky jazyk':
-            return Language.SLOVAK
+        if oriented.lower() == 'áno' or oriented.lower() == 'yes':
+            return True
+        elif oriented.lower() == 'nie' or oriented.lower() == 'no':
+            return False
         else:
-            raise ValueError(f"Unknown language: {language}")
-
-    def _extract_data_from_page(self, soup: BeautifulSoup) -> dict:
-        """
-        Extracts data from the HTML page content.
-
-        :param soup: Parsed HTML content.
-        :return: Dictionary containing study programme data.
-        """
-        data = {}
-        data.update(self._extract_data_from_section(soup, 'div', 'col-md-8 main'))
-        data.update(self._extract_data_from_section(soup, 'div', 'col-md-4 aside'))
-        return data
-
-    def _extract_data_from_section(self, soup: BeautifulSoup, tag: str, class_name: str) -> dict:
-        """
-        Extracts table data from a specific section (tag and class).
-
-        :param soup: Parsed HTML content.
-        :param tag: Tag name where the table is located.
-        :param class_name: Class name where the table is located.
-        :return: Dictionary with extracted key-value pairs.
-        """
-        section = soup.find(tag, class_=class_name)
-        if not section:
-            return {}
-
-        table = section.find('table', class_='table')
-        if not table:
-            return {}
-
-        return self._extract_table_data(table)
+            raise ValueError(f"Unknown professionally oriented value: {oriented}")
 
     @staticmethod
-    def _extract_table_data(table):
+    def _map_to_joint_study_program(joint_program: str) -> bool:
         """
-        Extracts data from an HTML table.
+        Maps a string to a Joint study program bool value
 
-        :param table: BeautifulSoup object representing the HTML table.
-        :return: Dictionary with extracted key-value pairs.
+        :param joint_program: String to map.
+        :return: bool value ("True" or "False").
         """
-        data = {}
-        for row in table.find_all('tr'):
-            th = row.find('th')
-            td = row.find('td')
-
-            if th and td:
-                key = th.text.strip()
-                value = td.text.strip()
-                if key not in data:
-                    data[key] = value
-        return data
-
-    @classmethod
-    def _get_page_url(cls, data: dict) -> str:
-        study_programme_code = data.get("Kód:", "") or data.get("Code:", "")
-        language = "en" if "Code:" in data else "sk"
-        return cls._PROGRAMME_PAGE_URL_TEMPLATE.format(code=study_programme_code, lang=language)
-
-    def _create_study_programme(self, data) -> TukeStudyProgramme:
-        """
-            Creates a TukeStudyProgramme object from the extracted data.
-
-            :param data: Dictionary containing study programme data.
-            :return: TukeStudyProgramme object.
-        """
-        return TukeStudyProgramme(
-            page_url=self._get_page_url(data),
-            page_language=self._map_to_language(data.get("Jazyky poskytovania:", "") or
-                                                data.get("Languages of provision:", "")),
-            name=data.get("Študijný program:", "") or data.get("Study program:", ""),
-            programme_code=int(data.get("Kód:", "") or data.get("Code:", "")),
-            study_field=data.get("Študijný odbor:", "") or data.get("Field of study:", ""),
-            level_of_degree=int((data.get("Stupeň štúdia:", "") or data.get("Level of study:", "")).replace(".", "")),
-            study_form=self._map_to_study_form(data.get("Forma štúdia:", "") or data.get("Form of study:", "")),
-            degree=self._map_to_degree(data.get("Stupeň štúdia:", "") or data.get("Level of study:", "")),
-            length_of_study_in_years=int(data.get("Dĺžka štúdia:", "") or data.get("Length of study:", "")),
-            professionally_oriented=self._map_to_professionally_oriented(data.get("Profesijne orientovaný:", "")
-                                                                         or data.get("Professionally oriented:", "")),
-            joint_study_program=self._map_to_joint_study_program(data.get("Spoločný študijný program:", "")
-                                                                 or data.get("Common study program:")),
-            languages_of_delivery=self._map_to_language(data.get("Jazyky poskytovania:", "") or
-                                                        data.get("Languages of provision:", "")),
-            description="",
-            learning_objectives="",
-            main_learning_outcomes="",
-            faculty=data.get("Fakulta:", "") or data.get("Faculty:", "")
-        )
+        if joint_program.lower() == 'áno' or joint_program.lower() == 'yes':
+            return True
+        elif joint_program.lower() == 'nie' or joint_program.lower() == 'no':
+            return False
+        else:
+            raise ValueError(f"Unknown Joint study program value: {joint_program}")
