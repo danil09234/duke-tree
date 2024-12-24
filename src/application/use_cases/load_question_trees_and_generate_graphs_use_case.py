93b4c9d941c5440f2d8334f3bcf5f1aa4e126695
Danylo Zahorulko <dailzag05@gmail.com>
2024-12-24 17:52:01 +0200

    Add `LoadQuestionTreesAndGenerateGraphsUseCase` use case

---
+++ src/application/use_cases/load_question_trees_and_generate_graphs_use_case.py
@@ -0,0 +1,26 @@
+from src.application.interfaces import QuestionTreeGraphGenerator
+from src.application.interfaces import Savable, GetAllRepository
+from src.domain.entities.question_tree import QuestionsTree
+
+
+class LoadQuestionTreesAndGenerateGraphsUseCase[StudyProgrammeData]:
+    def __init__(
+            self,
+            questions_tree_storage: GetAllRepository[QuestionsTree[StudyProgrammeData]],
+            graph_generator: QuestionTreeGraphGenerator[StudyProgrammeData],
+            graph_storage: Savable[str]
+    ) -> None:
+        self._questions_tree_repository = questions_tree_storage
+        self._graph_generator = graph_generator
+        self._graph_storage = graph_storage
+
+    async def __call__(self) -> None:
+        """
+        Loads question trees, generates graphs and saves them.
+
+        :return: None
+        """
+
+        question_trees = await self._questions_tree_repository.get_all()
+        graphs = [self._graph_generator.generate(question_tree) for question_tree in question_trees]
+        await self._graph_storage.save_multiple(graphs)
