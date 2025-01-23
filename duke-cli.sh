45e52482e4dcf7dd6f04c825c3ed708f85475c05
Danylo Zahorulko <dailzag05@gmail.com>
2025-01-23 20:29:54 +0200

    Update `duke-cli.sh`

--- duke-cli.sh
+++ duke-cli.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-docker compose down --remove-orphans -v
 docker compose build
 
 volumes=()
+env_vars=()
+
+if [ -f .env ]; then
+  while IFS='=' read -r key value; do
+    [[ -z "$key" || "$key" == \#* ]] && continue
+    env_vars+=("-e" "$key=$value")
+  done < .env
+fi
 
 for arg in "$@"; do
   if [ -f "$arg" ]; then
     host_dir=$(dirname "$(realpath "$arg")")
-    volumes+=("-v" "${host_dir}:${host_dir}:ro")
+    volumes+=("-v" "${host_dir}:${host_dir}:rw")
   fi
 done
 
-docker compose run "${volumes[@]}" duke-cli "$@"
+docker compose run "${env_vars[@]}" "${volumes[@]}" duke-cli "$@"
