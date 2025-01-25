ff32fa6166aff9f6ebf9bf68aed99c1b600b3931
Danylo Zahorulko <dailzag05@gmail.com>
2025-01-26 00:11:04 +0200

    Add `cors` middleware

--- src/infrastructure/api/app.py
+++ src/infrastructure/api/app.py
@@ -1,11 +1,20 @@
 import asyncio
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from src.infrastructure.api.routers.question_tree_api import router as qt_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(qt_router, prefix="/api")
 
